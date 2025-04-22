3ff3f64512f851ef3bf23251d7b573dad3939128
Frederik Berlaen <frederik@typemytype.com>
2025-04-22 15:13:05 +0200

    add zips for each webfont

--- scripts/zipper.py
+++ scripts/zipper.py
@@ -16,16 +16,16 @@ class Controller(ezui.WindowController):
 
     def build(self):
         content = """
-        = TwoColumnForm     
-   
+        = TwoColumnForm
+
         : WWW Repo:
         * HorizontalStack      @repoStack
         > (/.)                 @repoRoot
         > ( Clear )            @repoRootClear
-                
+
         : Typefaces:
         ( ...)                @typefaces
-        
+
         =---=
         ( Zip it! )  @ zipit
         """
@@ -35,16 +35,16 @@ class Controller(ezui.WindowController):
             ),
             repoStack=dict(
                 distribution="fillProportionally"
-            ),  
+            ),
             sourceStack=dict(
                 distribution="fillProportionally"
-            ),           
+            ),
             repoRoot=dict(
-                path=getExtensionDefault(repoRootDefaultKey, str(Path.home())), 
+                path=getExtensionDefault(repoRootDefaultKey, str(Path.home())),
                 pathStyle="popUp",
             ),
             sourceRoot=dict(
-                path="/", 
+                path="/",
                 pathStyle="popUp",
             ),
         )
@@ -56,35 +56,35 @@ class Controller(ezui.WindowController):
             controller=self
         )
         self.setTypefaces()
-    
+
     def started(self):
         self.w.open()
-        
+
     def repoRootCallback(self, sender):
         setExtensionDefault(repoRootDefaultKey, sender.get())
         self.setTypefaces()
-    
+
     def repoRootClearCallback(self, sender):
         removeExtensionDefault(repoRootDefaultKey)
         self.setTypeface()
         self.w.getItem("repoRoot").set(str(Path.home()))
-    
+
     def zipitCallback(self, sender):
         self.showGetFolder(
             messageText="Generated source:",
             allowsMultipleSelection=False,
             callback=self.zipper
-        )        
-    
+        )
+
     # helpers
-    
+
     def zipper(self, source):
         if not source:
             return
         source = Path(source[-1])
-        zipped = source / "zipped"        
+        zipped = source / "zipped"
         zipped.mkdir(parents=True, exist_ok=True)
-    
+
         root = Path(self.w.getItem("repoRoot").get())
         typeface = self.w.getItem("typefaces").getItem()
         typefaceRoot = root / "_typefaces" / typeface
@@ -95,20 +95,20 @@ class Controller(ezui.WindowController):
             for subfolder in (staticFontsRoot, variableFontsRoot):
                 fontRoot = source / subfolder
                 fontPaths.extend(fontRoot.glob(f'*.{fileType}'))
-        
+
         specimenPaths = list(typefaceRoot.glob("*Specimen*"))
         desktopEulaPath = list(eulaRoot.glob("*EULA-Desktop*"))
 
         if fontPaths and desktopEulaPath:
             desktopEulaPath = desktopEulaPath[0]
-        
-            # desktop family    
+
+            # desktop family
             with ZipFile(zipped / f'desktop-{typeface}.zip', 'w') as zipFile:
                 for fontPath in fontPaths:
                     zipFile.write(fontPath, arcname=fontPath.name)
-        
+
                 zipFile.write(desktopEulaPath, arcname=desktopEulaPath.name)
-                
+
                 for specimentPath in specimenPaths:
                     zipFile.write(specimentPath, arcname=specimentPath.name)
             # desktop style
@@ -116,39 +116,50 @@ class Controller(ezui.WindowController):
                 with ZipFile(zipped / f'desktop-{fontPath.name[:-4]}.zip', 'w') as zipFile:
                     zipFile.write(fontPath, arcname=fontPath.name)
                     zipFile.write(desktopEulaPath, arcname=desktopEulaPath.name)
-                    
+
                     for specimentPath in specimenPaths:
                         zipFile.write(specimentPath, arcname=specimentPath.name)
-    
+
         webFontPaths = []
         for subfolder in (staticFontsRoot, variableFontsRoot):
             fontRoot = source / webFontsRoot  / subfolder
             webFontPaths.extend(fontRoot.glob(f'*.woff2'))
-            
+
         webDocuments = list(eulaRoot.glob("*EULA-Web*"))
         if webDocuments and webFontPaths:
-            # webfonts
+            # webfonts family
             with ZipFile(zipped / f'webfonts-{typeface}.zip', 'w') as zipFile:
                 for webFontPath in webFontPaths:
                     zipFile.write(webFontPath, arcname=webFontPath.name)
-            
+
                 for webDocument in webDocuments:
                     zipFile.write(webDocument, arcname=webDocument.name)
-                
+
                 for specimentPath in specimenPaths:
                     zipFile.write(specimentPath, arcname=specimentPath.name)
-        
+
+            # webfonts style
+            for webFontPath in webFontPaths:
+                with ZipFile(zipped / f'webfonts-{fontPath.name[:-4]}.zip', 'w') as zipFile:
+                    zipFile.write(webFontPath, arcname=webFontPath.name)
+
+                    for webDocument in webDocuments:
+                        zipFile.write(webDocument, arcname=webDocument.name)
+
+                    for specimentPath in specimenPaths:
+                        zipFile.write(specimentPath, arcname=specimentPath.name)
+
         print("done")
 
     def setTypefaces(self):
         root = self.w.getItem("repoRoot").get()
         typefaces = self.w.getItem("typefaces")
-        
+
         typefaceRoot = Path(root) / "_typefaces"
         names = []
         if typefaceRoot.exists():
-            names = [folder.name for folder in typefaceRoot.iterdir() if folder.is_dir()]    
+            names = [folder.name for folder in typefaceRoot.iterdir() if folder.is_dir()]
         typefaces.setItems(names)
-         
-        
-Controller()    
+
+
+Controller()
